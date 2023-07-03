009822473ea207fb7a33cc03062c451568632b40
peseniy <catkitkat.ars@yandex.ru>
2023-07-03 21:10:34 +0400

    offset

--- DataHandler.php
+++ DataHandler.php
@@ -4,16 +4,19 @@ class DataHandler {
     private $fp;
     private $fileName;
 
-
-
-
-
-
+    private $fp_offset;
+    public $offset;
+    public $micro_time;
 
     public function __construct($fileName)
     {
         $this->fileName = __DIR__ . '/' . $this->checkFileName($fileName);
         $this->fp = fopen($this->fileName, 'r+');
+
+        $this->fp_offset = fopen(__DIR__ . '/' . $this->checkFileName('offset.txt'), 'r+');
+        $this->offset = $this->offset_file_to_array();
+
+        // $this->
     }
     
     private function checkFileName(string $fileName):string {
@@ -23,9 +26,40 @@ class DataHandler {
         return $fileName;
     }
 
-    private function combine($row) {
+    private function offset_file_to_array() {
+        $result = [];
+        while ($row = fgets($this->fp_offset)) { 
+            $row = $this->myExplode('|', $row);
+            for($i = 1; $i < count($row); $i++) {
+                if($i == 1) {
+                    $result[$row[0]] = [];
+                }
+                array_push($result[$row[0]], $row[$i]);
+            }
+        }
+        return $result;
+    }
+
+    
+
+    public function myExplode($sep, $str){
+
+        $sepQuote = preg_quote($sep); 
+
+        $str = preg_split("/(?<!$sepQuote)$sepQuote(?!$sepQuote)/", $str);
+
+        foreach($str as $key => $value) {
+            if(str_contains($value, ("$sep"."$sep"))) {
+                $str[$key] = strtr($value, [("$sep"."$sep") => "$sep"]);
+            }
+        }
+
+        return $str;
+    }
+
+    public function combine($row) {
         $keys = ['name', 'phone', 'email', 'city'];
-        $processedRow = array_combine($keys, explode(' ', $row));
+        $processedRow = array_combine($keys, $this->myExplode('|', $row));
         return $processedRow;
     }
 
@@ -34,11 +68,11 @@ class DataHandler {
         return true && $compare;
     }
 
-    private function or($compare){
+    private function or($compare): bool{
         return false || $compare;
     }
 
-    public function match($row, $query): bool {
+    private function match($row, $query): bool {
         $left = NULL;
         $right = NULL;
         $result = false;
@@ -86,31 +120,111 @@ class DataHandler {
         return $result;
     }
 
+    public function offset_select($name) {
+        foreach($this->offset as $key => $value) {
+            if($key == $name){
+                return $this->offset[$key];
+            }
+        }
+    }
+
+    public function offset_insert($name, $offset) {
+        $offset = strval($offset);
+        $name = trim($name);
+        $offset_check = true;
+        $offset_string = '';
+        foreach($this->offset as $key => $value) {
+            if ($key == $name) {
+                $offset_check = false;
+                array_push($this->offset[$key], $offset);
+            }
+        }
+        if($offset_check) {
+            $this->offset[$name] = [$offset];
+        }
+        
+        $file_name = __DIR__ . "/" . $this->checkFileName('offset.txt');
+        $fp = fopen($file_name, 'r+');
+
+        foreach($this->offset as $key => $value) {
+            fwrite($fp,($key . '|' . implode('|', $this->offset[$key]) . "\n"));
+        }
+    }
+
+    public function offset_delete() {
+        
+    }
+
+    public function offset_update() {
+
+    }
+
+
     public function select($filter){
         $processedData = [];
         if(!$filter){
             while ($row = fgets($this->fp)) { 
+                if($row[0] == '0') {
+                    continue;
+                }
+                $row = preg_replace("/^.{2}/", '', $row);
                 array_push($processedData, $this->combine($row));
             }
         } 
         else 
         {
-            while ($row = fgets($this->fp)) { 
-                $combinedRow = $this->combine($row);
+            if($filter['operator'] == '=' && $filter['left'] == 'name') {
+                $offsets = $this->offset_select($filter['right']);
 
-                if($this->match($combinedRow, $filter)) {
+                foreach($offsets as $value) {
+                    fseek($this->fp, $value);
+                    $row = fgets($this->fp);
+                    $row = preg_replace("/^.{2}/", '', $row);
+                    $combinedRow = $this->combine($row);
                     array_push($processedData, $combinedRow);
                 }
             }
+            else 
+            {
+                while ($row = fgets($this->fp)) { 
+                    if($row[0] == '0') {
+                        continue;
+                    }
+                    $row = preg_replace("/^.{2}/", '', $row);
+                    $combinedRow = $this->combine($row);
+    
+                    if($this->match($combinedRow, $filter)) {
+                        array_push($processedData, $combinedRow);
+                    }
+                }
+            }
         }
+        
+        // !!!
+        $this->micro_time = ' microtime(' . microtime() . ')' . ' memory(' . memory_get_usage() . ')';
+
         return $processedData;
     }
 
-    public function insert($filter){
-        foreach($filter as $key => $value) {
+    public function insert($values){
+        $name = '';
+        
+        foreach($values as $key => $value) {   
             $value = trim($value);
+
+            if($key == 'name'){
+                $name .= $values[$key];
+            }
+
+            if(str_contains($value, '|')) {
+                $values[$key] = strtr($value, ['|' => '||']);
+            }
         }
-        file_put_contents($this->fileName, ("\n" . implode(' ', $filter)), FILE_APPEND);
+        
+        fseek($this->fp, 0, SEEK_END);
+        $offset = ftell($this->fp);
+        $this->offset_insert($name, $offset);
+        fwrite($this->fp,("1|" . implode('|', $values) . "\n"));
     }
 
     public function delete($filter) {
--- index.php
+++ index.php
@@ -22,7 +22,6 @@ catch (Exception $e)
 }
 
 
-
 // Проверить работу методов, которые записаны в класс DataHandler
 
 
@@ -56,34 +55,192 @@ catch (Exception $e)
 //     'city' => 'Москва'
 // ]);
 
-  $rows = $dataHandler->select(
-  [
-    'operator' => 'or',
-    'left' => [
-        'operator' => '=',
-        'left' => 'name',
-        'right' => 'Egor',
-    ],
-    'right' => [
-        'operator' => '=',
-        'left' => 'city',
-        'right' => 'Москва',
-    ],
-]); 
 
+
+// $a = $dataHandler->myExplode(' ', 'Egor 89005558423 egor@test4.com Нижний  Новгород');
+
+// var_dump($a);
+
+// $str = 'test hello  world';
+
+// function explode1($sep, $str) {
+//   $arr = [];
+//   $str2 = '';
+//   $marker = false;
+
+//   for($i = 0; $i < strlen($str); $i++) {
+//     if($str[$i] != $sep && $marker == false) { // Если элемент не равен пробелу и маркер равен отрицанию
+//       $str2 .= $str[$i];                      // Присвой это значение к другим значениям в строке
+//     }
+//     if($str[$i] == $sep && $marker == true) { // Если элемент равен пробелу и маркер равен согласию
+//       $str2 .= $sep;                          // Присвои строке пробел 
+//       $marker = false;                        // Маркер переведи в отрицание
+//       continue;                               // Пропусти итерацию
+//     }
+//     if($str[$i] == $sep) {
+//       $marker = true;
+//     }
+//     if($str[$i] != $sep && $marker == true) {
+//       array_push($arr, $str2);
+//       $str2 = $str[$i];
+//       $marker = false;
+//     }
+//   }
+//   if($str2) array_push($arr, $str2);
+//   return $arr;
+// }
+
+
+
+// $a = preg_match_all('/кошка/', $str);
+
+// $b = preg_match_all('/\d+/', $str, $m);
+
+// $c = preg_match_all('/\w+@\w+\.\w+/', $str, $m);
+
+// =========================
+
+// $c = preg_match_all('/\+\d+\(\d+\)\d+/', $str, $m);
+
+// $str = 'https://cat.com'; // !!!!!!!!
+
+// $c = preg_match_all('/https?:\/\/\S+/', $str, $m); // !!!!!!!!
+
+// $str = 'duble notduble duble';
+
+// $a = preg_match_all('/\b(\w+)\b(?=.*\b\1\b)/', $str, $m);
+
+// var_dump($m);
+
+
+
+// $str = "Глеб|8(947)4838372|address9@mail.info|Москва";
+
+$rows = $dataHandler->select([
+  'operator' => '=',
+  'left' => 'name',
+  'right' => 'Глеб'
+]);
+
+
+
+
+var_dump($dataHandler->micro_time);
+
+
+// $rows = $dataHandler->select(
+//   [
+//     'operator' => 'and',
+//     'left' => [
+//         'operator' => '=',
+//         'left' => 'name',
+//         'right' => 'Виктория',
+//     ],
+//     'right' => [
+//         'operator' => '=',
+//         'left' => 'phone',
+//         'right' => '8(949)7102519',
+//     ],
+//   ]); 
+
+
+
+
+// $ftell = $dataHandler->insert([ // Done
+//   'name' => 'Князь',
+//   'phone' => '8(947)4838372',
+//   'email' => 'address9@mail.info',
+//   'city' => 'Москва'
+// ]);
+// $ftell = $dataHandler->insert([ // Done
+//   'name' => 'Глеб',
+//   'phone' => '8(947)4838372',
+//   'email' => 'address9@mail.info',
+//   'city' => 'Москва'
+// ]);
+// $ftell = $dataHandler->insert([ // Done
+//   'name' => 'Жлоб',
+//   'phone' => '8(947)4838372',
+//   'email' => 'address9@mail.info',
+//   'city' => 'Москва'
+// ]);
+
+
+
+// $insert = $dataHandler->offset_insert('ivan', '123456');
+// $b = $dataHandler->offset_insert('ivan', '654321');
+// // var_dump($ftell);
+// $a = $dataHandler->offset;
+
+// var_dump($a);
+
+
+
+// $rows = $dataHandler->select(
+//   []);
 
 // $arr = ['name'=>'Egor', 'city' => 'Москва'];
 
-
 // var_dump($arr);
 
+// $names = [
+//   "Василий",
+//   "Борис",
+//   "Никита",
+//   "Константин",
+//   "Юлия",
+//   "Кристина",
+//   "Виктория",
+//   "Глеб",
+//   "Евгений",
+//   "Тамара"
+// ];
 
-// $dataHandler->insert($filePath,[ // Done
-//     'name' => 'Egor',
-//     'phone' => '89005558423',
-//     'email' => 'egor@test4.com',
-//     'city' => 'Москва'
-// ]);
+// $mails = [
+//   "address1@mail.info",
+//   "address2@mail.info",
+//   "address3@mail.info",
+//   "address4@mail.info",
+//   "address5@mail.info",
+//   "address6@mail.info",
+//   "address7@mail.info",
+//   "address8@mail.info",
+//   "address9@mail.info",
+//   "address10@mail.info"
+// ];
+
+// $citys = [
+//   "Москва",
+//   "Санкт-Петербург",
+//   "Новосибирск",
+//   "Екатеринбург",
+//   "Нижний Новгород",
+//   "Казань",
+//   "Челябинск",
+//   "Омск",
+//   "Самара",
+//   "Ростов-на-Дону"
+// ];
+
+// function arrayGenerate($a, $c, $d)
+// {
+//   $str = "8" . '(' . rand(900, 999) . ')' . rand(1111111, 9999999);
+
+//   return $a[array_rand($a)] ."|". $str ."|". $c[array_rand($c)] ."|". $d[array_rand($d)];
+// }
+
+
+
+
+// var_dump($str);
+
+// for($i = 0; $i < 10000; $i++) {
+//   $dataHandler->insert(
+//     $dataHandler->combine(arrayGenerate($names, $mails, $citys))
+//   );
+// }
+
+// var_dump($dataHandler->combine(arrayGenerate($names, $phones, $mails, $citys)));
 
 // $dataHandler->delete($filePath, [ // Done
 //     'name' => 'Василий',
